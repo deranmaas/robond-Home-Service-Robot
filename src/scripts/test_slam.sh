79083c99e08bf1d9191abd5072bca607ef99b4e8
Deran Maas <deranmaas@gmail.com>
2022-01-09 14:16:46 -0800

    working test_slam.sh script

--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -2,18 +2,29 @@
 
 DIR="$( cd "$(dirname $(realpath $0))/../.." >/dev/null 2>&1 && pwd )"
 
-xterm -e  "
+xterm -hold -e  "
+cd $DIR;
+source devel/setup.bash;
 export TURTLEBOT_GAZEBO_WORLD_FILE='$DIR/src/my_robot/worlds/myoffice_new.world';
 roslaunch turtlebot_gazebo turtlebot_world.launch" &
 
 sleep 5
 
-xterm -e "
+xterm -hold -e "
+cd $DIR;
+source devel/setup.bash;
 roslaunch turtlebot_gazebo gmapping_demo.launch" &
 
 sleep 5
 
-#xterm -hold -e "
-#cd $DIR
-#source devel/setup.bash
-#roslaunch turtlebot_teleop keyboard_teleop.launch" &
+xterm -hold -e "
+cd $DIR;
+source devel/setup.bash;
+roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+
+sleep 5
+
+xterm -hold -e "
+cd $DIR;
+source devel/setup.bash;
+roslaunch turtlebot_teleop keyboard_teleop.launch" &
