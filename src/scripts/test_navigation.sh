3ccc63495624ea5984e5f9198f0b1ac60560717e
Deran Maas <deranmaas@gmail.com>
2022-01-10 08:16:41 -0800

    localization and navigation

---
+++ src/scripts/test_navigation.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+DIR="$( cd "$(dirname $(realpath $0))/../.." >/dev/null 2>&1 && pwd )"
+
+xterm -e  "
+cd $DIR;
+source devel/setup.bash;
+export TURTLEBOT_GAZEBO_WORLD_FILE='$DIR/src/my_robot/worlds/myoffice_new.world';
+roslaunch turtlebot_gazebo turtlebot_world.launch" &
+
+sleep 5
+
+xterm -e "
+cd $DIR;
+source devel/setup.bash;
+export TURTLEBOT_GAZEBO_MAP_FILE='$DIR/src/my_robot/maps/myoffice.yaml';
+roslaunch turtlebot_gazebo amcl_demo.launch" &
+
+sleep 5
+
+xterm  -e "
+cd $DIR;
+source devel/setup.bash;
+roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+
