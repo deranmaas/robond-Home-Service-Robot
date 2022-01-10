b161e0949b8982d11b61f076a4d7ca8a5346c06a
Deran Maas <deranmaas@gmail.com>
2022-01-10 15:26:23 -0800

    simple add_marker

--- src/scripts/add_marker.sh
+++ src/scripts/add_marker.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$(dirname $(realpath $0))/../.." >/dev/null 2>&1 && pwd )"
 xterm -e  "
 cd $DIR;
 source devel/setup.bash;
-export TURTLEBOT_GAZEBO_WORLD_FILE='$DIR/src/my_robot/worlds/myoffice_new.world';
+export TURTLEBOT_GAZEBO_WORLD_FILE='$DIR/src/world/myoffice_new.world';
 roslaunch turtlebot_gazebo turtlebot_world.launch" &
 
 sleep 5
@@ -13,7 +13,7 @@ sleep 5
 xterm -e "
 cd $DIR;
 source devel/setup.bash;
-export TURTLEBOT_GAZEBO_MAP_FILE='$DIR/src/my_robot/maps/myoffice.yaml';
+export TURTLEBOT_GAZEBO_MAP_FILE='$DIR/src/map/myoffice.yaml';
 roslaunch turtlebot_gazebo amcl_demo.launch" &
 
 sleep 5
@@ -28,6 +28,6 @@ sleep 5
 xterm -hold -e "
 cd $DIR;
 source devel/setup.bash;
-rosrun add_markers add_markers" &
+rosrun add_markers add_markers_simple" &
 
 
